e4de3ceb8955ab5d824abd1e15dff1525cceaac9
Carlos Eduardo Dal Molin <carlosdudu164@gmail.com>
2024-11-20 23:59:18 -0300

    feat: #CERUTF-121 - Criado a tela do Scanner

--- src/app/event/detail/index.tsx
+++ src/app/event/detail/index.tsx
@@ -1,15 +1,40 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import { router } from 'expo-router';
 import React from 'react'
+import { useCameraPermissions } from "expo-camera"
+
+import Button from '@/src/components/button'
 
 export default function EventDetail() {
-  return (
-    <View className='flex-1 items-center justify-center'>
-      <Text>EventDetail</Text>
+  const [permission, requestPermission] = useCameraPermissions();
 
-      <TouchableOpacity onPress={() => router.back()}>
-        <Text>Go to Home</Text>
-      </TouchableOpacity>
+  async function readQRCode() {
+    if(permission?.granted) {
+      router.push('/scanner')
+    } else {
+      if((await requestPermission())?.granted) {
+        router.push('/scanner')
+      }
+    }
+  }
+
+  
+  return (
+    <View className='flex-1 h-full'>
+      <View
+        style={{height:'92%'}}
+        className='items-center justify-center h-5/6 bg-slate-100'>
+        <Text>EventDetail</Text>
+
+        <TouchableOpacity onPress={() => router.replace('/')}>
+          <Text>Go to Home</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{height:'8%'}}
+        className='items-center justify-center h-36 bg-white'>
+        <Button action={readQRCode}/>
+      </View>
     </View>
   )
 }
